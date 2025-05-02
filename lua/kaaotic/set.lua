37c159469aba76a9c0fcaf14d102556da15aca5f
pegomez5 <pegomez@ursinus.edu>
2025-05-02 11:45:01 -0400

    Added copilot + whatever tf i did months ago

--- lua/kaaotic/set.lua
+++ lua/kaaotic/set.lua
@@ -8,7 +8,13 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+--vim.opt.breakat = "80"
+--vim.opt.columns = 80
+--vim.opt.textwidth = 80
+--vim.opt.wrapmargin = 80
+--vim.opt.linebreak = true
 vim.opt.wrap = false
+vim.opt.colorcolumn = "80"
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
