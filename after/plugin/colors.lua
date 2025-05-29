4d7bfec585008a0e337c90a22e69cdda611c03da
pegomez5 <pegomez@ursinus.edu>
2025-05-29 16:33:12 -0400

    Reverted to before copilot

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,9 +3,9 @@ function ColorMyPencils(color)
     vim.cmd.colorscheme(color)
 
     -- transparent background
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+--    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+--    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+--    vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+--    vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
 end
 ColorMyPencils()
